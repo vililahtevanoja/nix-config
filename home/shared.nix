7cfec2e5e64eab11048308817b9695701ceaae50
Vili Lähtevänoja <3448875+vililahtevanoja@users.noreply.github.com>
2025-12-13 19:05:47 +0200

    use cargo-nextest for tests

--- home/shared.nix
+++ home/shared.nix
@@ -9,14 +9,12 @@ let
   starship-jj = pkgs.rustPlatform.buildRustPackage rec {
     pname = "starship-jj";
     version = "0.7.0"; # Check for latest version on crates.io
-
     src = pkgs.fetchCrate {
       inherit pname version;
       hash = "sha256-oisz3V3UDHvmvbA7+t5j7waN9NykMUWGOpEB5EkmYew";
     };
-
     cargoHash = "sha256-NNeovW27YSK/fO2DjAsJqBvebd43usCw7ni47cgTth8";
-
+    useNextest = true;
   };
 in
 {
