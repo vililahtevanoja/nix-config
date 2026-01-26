ac40a1536cef040f7610ed5d4d61ee5e9aa8cf7a
Vili Lähtevänoja <3448875+vililahtevanoja@users.noreply.github.com>
2026-01-26 09:38:19 +0200

    refactor and do prep work for kiro-cli but hold the update for now

--- home/aarch64-darwin.nix
+++ home/aarch64-darwin.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... }:
+{ nixpkgs, pkgs, lib, ... }:
 {
   home.sessionPath = [
     "$HOME/bin"
   ];
 
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "kiro-cli"
+    ];
+
   # macOS (Apple Silicon) specific settings
   home.packages = with pkgs; [
     colima
+
+    # LLM
+    amazon-q-cli
+    # kiro-cli # hold off for now
   ];
 
   # Shared settings for darwin (e.g., macOS)
