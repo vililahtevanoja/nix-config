164c6218ad9fcc2a0922c7fcf6e06b76385f618b
Vili Lähtevänoja <3448875+vililahtevanoja@users.noreply.github.com>
2026-04-03 19:28:15 +0300

    better home manager managed zsh binary reference

--- home/shared-linux.nix
+++ home/shared-linux.nix
@@ -4,6 +4,9 @@
   lib,
   ...
 }:
+
+# path to home-manager-managed zsh binary
+let zshBin = "${config.home.profileDirectory}/bin/zsh"; in
 {
   home.packages = with pkgs; [
 
@@ -11,7 +14,7 @@
 
   home.sessionVariables = {
     # Keep child processes aligned with the Home Manager-managed zsh binary.
-    SHELL = "${config.home.profileDirectory}/bin/zsh";
+    SHELL = zshBin;
   };
 
   home.sessionPath = [
@@ -22,11 +25,11 @@
 
   programs.zsh.initContent = lib.mkBefore ''
     # Re-exec into the Nix-managed zsh before loading native plugins.
-    if [[ -z ''${ZSH_NIX_REEXEC-} && -x ${config.home.profileDirectory}/bin/zsh ]]; then
+    if [[ -z ''${ZSH_NIX_REEXEC-} && -x ${zshBin} ]]; then
       current_zsh=$(readlink -f /proc/$$/exe 2>/dev/null || true)
-      if [[ $current_zsh != ${config.home.profileDirectory}/bin/zsh ]]; then
+      if [[ $current_zsh != ${zshBin} ]]; then
         export ZSH_NIX_REEXEC=1
-        exec ${config.home.profileDirectory}/bin/zsh
+        exec ${zshBin}
       fi
     fi
   '';
