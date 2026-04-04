b7f1d50d191ba97f19eff25c608f49bb02a573b7
Vili Lähtevänoja <3448875+vililahtevanoja@users.noreply.github.com>
2026-04-04 11:50:22 +0300

    refactor zsh dotdir references

--- home/shared.nix
+++ home/shared.nix
@@ -235,106 +235,110 @@ in
       };
     };
   };
-  programs.zsh = {
-    enable = true;
-    # Use XDG location for zsh config files.
-    dotDir = "${config.xdg.configHome}/zsh";
-    syntaxHighlighting.enable = false;
-    autosuggestion.enable = true;
-    historySubstringSearch.enable = true;
-    enableCompletion = true;
-    antidote = {
+  programs.zsh =
+    let
+      # Use XDG location for zsh config files.
+      zshDotDir = "${config.xdg.configHome}/zsh";
+    in
+    {
       enable = true;
-      plugins = [
+      dotDir = zshDotDir;
+      syntaxHighlighting.enable = false;
+      autosuggestion.enable = true;
+      historySubstringSearch.enable = true;
+      enableCompletion = true;
+      antidote = {
+        enable = true;
+        plugins = [
+          ''
+            mattmc3/ez-compinit
+            zsh-users/zsh-autosuggestions
+            ohmyzsh/ohmyzsh path:lib/git.zsh
+            ohmyzsh/ohmyzsh path:plugins/git
+            ohmyzsh/ohmyzsh path:plugins/fzf
+            ohmyzsh/ohmyzsh path:plugins/direnv
+          ''
+        ];
+      };
+      shellAliases = common-shell-aliases // {
+        zshrc = "${lib.getExe pkgs.bat} ${zshDotDir}/.zshrc";
+        reload = ". ${zshDotDir}/.zshrc";
+      };
+      history = {
+        size = 1000000;
+        save = 1000000;
+        ignoreAllDups = true;
+        findNoDups = true;
+        ignoreSpace = true;
+      };
+      # setup .zshrc contents
+      initContent = lib.mkMerge [
+        # ensure stale starship keymap widget wrappers are removed before starship init
+        # ref: https://github.com/starship/starship/issues/3418
+        # potential fix: https://github.com/starship/starship/pull/6398
+        (lib.mkBefore ''
+          if [[ "''${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+                "''${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+            zle -N zle-keymap-select ""
+          fi
+        '')
+        # Prettified `time` command output
         ''
-          mattmc3/ez-compinit
-          zsh-users/zsh-autosuggestions
-          ohmyzsh/ohmyzsh path:lib/git.zsh
-          ohmyzsh/ohmyzsh path:plugins/git
-          ohmyzsh/ohmyzsh path:plugins/fzf
-          ohmyzsh/ohmyzsh path:plugins/direnv
+          if [[ `uname` == Darwin ]]; then
+            MAX_MEMORY_UNITS=KB
+          else
+            MAX_MEMORY_UNITS=MB
+          fi
+
+          TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
+          'avg shared (code):         %X KB'$'\n'\
+          'avg unshared (data/stack): %D KB'$'\n'\
+          'total (sum):               %K KB'$'\n'\
+          'max memory:                %M '$MAX_MEMORY_UNITS''$'\n'\
+          'page faults from disk:     %F'$'\n'\
+          'other page faults:         %R'
+        ''
+        "source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh"
+        # setting to help with history
+        ''
+          setopt HIST_REDUCE_BLANKS
+        ''
+        # increase ulimit to reduce errors from too many open files
+        "ulimit -n 10240" # max is 10240
+        # jujutsu autocompletion
+        ''
+          autoload -U compinit
+          compinit
+          source <(${lib.getExe pkgs.jujutsu} util completion zsh)
+        ''
+        # fix Home and End button behavior
+        ''
+          bindkey '^[[H' beginning-of-line
+          bindkey '^[[F' end-of-line
+        ''
+        # utility functions
+        ''
+          # make directory and move there
+          mkcd() {
+            mkdir -p "$1" && cd "$1"
+          }
+          # find files fast
+          # e.g. `ff .md` to find markdown files
+          ff() {
+            rg --files -g "*$1*" -i
+          }
+          # view file tree
+          tre() {
+            tree -aC -I '.git|.jj|.turbo|.terraform|.idea|node_modules|vendor|__pycache__|cdk.out|coverage|.tanstack|temp|.cache|.direnv' --dirsfirst "$@" | less -FRNX
+          }
+        ''
+        # enable zsh-patina (fast zsh highlighter)
+        ''
+          # Reference the executable direcly
+          eval "$(${lib.getExe zsh-patina} activate)"
         ''
       ];
     };
-    shellAliases = common-shell-aliases // {
-      zshrc = "${lib.getExe pkgs.bat} ${config.xdg.configHome}/zsh/.zshrc";
-      reload = ". ${config.xdg.configHome}/zsh/.zshrc";
-    };
-    history = {
-      size = 1000000;
-      save = 1000000;
-      ignoreAllDups = true;
-      findNoDups = true;
-      ignoreSpace = true;
-    };
-    # setup .zshrc contents
-    initContent = lib.mkMerge [
-      # ensure stale starship keymap widget wrappers are removed before starship init
-      # ref: https://github.com/starship/starship/issues/3418
-      # potential fix: https://github.com/starship/starship/pull/6398
-      (lib.mkBefore ''
-        if [[ "''${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
-              "''${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
-          zle -N zle-keymap-select ""
-        fi
-      '')
-      # Prettified `time` command output
-      ''
-        if [[ `uname` == Darwin ]]; then
-          MAX_MEMORY_UNITS=KB
-        else
-          MAX_MEMORY_UNITS=MB
-        fi
-
-        TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
-        'avg shared (code):         %X KB'$'\n'\
-        'avg unshared (data/stack): %D KB'$'\n'\
-        'total (sum):               %K KB'$'\n'\
-        'max memory:                %M '$MAX_MEMORY_UNITS''$'\n'\
-        'page faults from disk:     %F'$'\n'\
-        'other page faults:         %R'
-      ''
-      "source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh"
-      # setting to help with history
-      ''
-        setopt HIST_REDUCE_BLANKS
-      ''
-      # increase ulimit to reduce errors from too many open files
-      "ulimit -n 10240" # max is 10240
-      # jujutsu autocompletion
-      ''
-        autoload -U compinit
-        compinit
-        source <(${lib.getExe pkgs.jujutsu} util completion zsh)
-      ''
-      # fix Home and End button behavior
-      ''
-        bindkey '^[[H' beginning-of-line
-        bindkey '^[[F' end-of-line
-      ''
-      # utility functions
-      ''
-        # make directory and move there
-        mkcd() {
-          mkdir -p "$1" && cd "$1"
-        }
-        # find files fast
-        # e.g. `ff .md` to find markdown files
-        ff() {
-          rg --files -g "*$1*" -i
-        }
-        # view file tree
-        tre() {
-          tree -aC -I '.git|.jj|.turbo|.terraform|.idea|node_modules|vendor|__pycache__|cdk.out|coverage|.tanstack|temp|.cache|.direnv' --dirsfirst "$@" | less -FRNX
-        }
-      ''
-      # enable zsh-patina (fast zsh highlighter)
-      ''
-        # Reference the executable direcly
-        eval "$(${lib.getExe zsh-patina} activate)"
-      ''
-    ];
-  };
 
   programs.fish = {
     enable = true;
